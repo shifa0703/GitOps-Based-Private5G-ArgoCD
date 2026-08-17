17f3c6eb81d42ba0c04c2dfe77a575bbc05fa473
shifa0703 <shifayousuff0703@gmail.com>
2026-08-17 21:00:20 +0530

    Fix UERANSIM UE gNB discovery

--- kubernetes/ueransim/start-ue.sh
+++ kubernetes/ueransim/start-ue.sh
@@ -2,4 +2,16 @@
 
 cd /opt/UERANSIM
 
+GNB_IP=$(getent hosts ueransim-gnb | awk '{print $1}' | head -n1)
+
+if [ -z "$GNB_IP" ]; then
+    echo "ERROR: Could not resolve ueransim-gnb"
+    exit 1
+fi
+
+echo "Using gNB IP: $GNB_IP"
+
+sed -i "/gnbSearchList:/,/uacAic:/ s/^[[:space:]]*- [0-9.]*$/  - ${GNB_IP}/" /opt/UERANSIM/config/open5gs-ue.yaml
+
+echo "Starting UERANSIM UE..."
 ./build/nr-ue -c config/open5gs-ue.yaml
